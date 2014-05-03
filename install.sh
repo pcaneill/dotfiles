060773b111fabda69188e58d5076614ee7ac5c4b
Pierre-Yves Caneill <py.caneill@gmail.com>
2014-05-03 21:08:45 +0200

    VIM: introducing vim configuration

--- install.sh
+++ install.sh
@@ -16,5 +16,16 @@ xfconf-query -c xfce4-desktop                                 \
 # emacs
 cp emacs/emacs ~/.emacs
 
+# vim
+mkdir vim/plugin
+mkdir vim/doc
+
+cp submodules/supertab/doc/supertab.txt vim/doc/supertab.txt
+cp submodules/supertab/plugin/supertab.vim vim/plugin/supertab.vim
+
+
+cp submodules/bufexplorer/doc/bufexplorer.txt vim/doc/bufexplorer.txt
+cp submodules/bufexplorer/plugin/bufexplorer.vim vim/plugin/bufexplorer.vim
+
 # git
 cp git/gitconfig ~/.gitconfig
