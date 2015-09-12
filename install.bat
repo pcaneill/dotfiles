e6d8a8181b6d74372d6e69f624b3636945d257cb
Pierre-Yves Caneill <pyc@moeryn.com>
2015-09-13 00:07:00 +0200

    INSTALL:  vim

--- install.bat
+++ install.bat
@@ -1,23 +1,36 @@
-md vim/bundle
-FOR /F "tokens=*" %%a in ('chdir') do SET moeryn_path=%%a
-echo %moeryn_path%
+:: {{{ Vim
 
-mklink /d vim\bundle\a                 %moeryn_path%\submodules\vim\a
-mklink /d vim\bundle\bufexplorer       %moeryn_path%\submodules\vim\bufexplorer
-mklink /d vim\bundle\ctrlp             %moeryn_path%\submodules\vim\ctrlp
-mklink /d vim\bundle\htlm5             %moeryn_path%\submodules\vim\htlm5
-mklink /d vim\bundle\nerdcommenter     %moeryn_path%\submodules\vim\nerdcommenter
-mklink /d vim\bundle\nerdtree          %moeryn_path%\submodules\vim\nerdtree
-mklink /d vim\bundle\seoul256          %moeryn_path%\submodules\vim\seoul256
-mklink /d vim\bundle\supertab          %moeryn_path%\submodules\vim\supertab
-mklink /d vim\bundle\tern              %moeryn_path%\submodules\vim\tern
-mklink /d vim\bundle\vim-airline       %moeryn_path%\submodules\vim\vim-airline
-mklink /d vim\bundle\vim-fugitive      %moeryn_path%\submodules\vim\vim-fugitive
-mklink /d vim\bundle\vim-gitgutter     %moeryn_path%\submodules\vim\vim-gitgutter
-mklink /d vim\bundle\vim-javascript    %moeryn_path%\submodules\vim\vim-javascript
-mklink /d vim\bundle\vim-man           %moeryn_path%\submodules\vim\vim-man
-mklink /d vim\bundle\vim-signature     %moeryn_path%\submodules\vim\vim-signature
-mklink /d vim\bundle\vim-spacebars     %moeryn_path%\submodules\vim\vim-spacebars
-::mklink /d vim\bundle\editorconfig-vim  %moeryn_path%\submodules\vim\editorconfig-vim
-::mklink /d vim\bundle\syntastic         %moeryn_path%\submodules\vim\syntastic
-::mklink /d vim\bundle\ultisnips         %moeryn_path%\submodules\vim\ultisnips
+setlocal
+
+FOR /F "tokens=*" %%a in ('chdir') do SET CONFIG_PATH=%%a
+set HOME_DIR="C:\Users\pycan"
+set VIM_INSTAL="C:\Vim"
+
+:: Loading plugin
+md vim/bundle
+mklink /d vim\bundle\a                   %CONFIG_PATH%\submodules\vim\a
+mklink /d vim\bundle\bufexplorer         %CONFIG_PATH%\submodules\vim\bufexplorer
+mklink /d vim\bundle\ctrlp               %CONFIG_PATH%\submodules\vim\ctrlp
+mklink /d vim\bundle\htlm5               %CONFIG_PATH%\submodules\vim\htlm5
+mklink /d vim\bundle\nerdcommenter       %CONFIG_PATH%\submodules\vim\nerdcommenter
+mklink /d vim\bundle\nerdtree            %CONFIG_PATH%\submodules\vim\nerdtree
+mklink /d vim\bundle\seoul256            %CONFIG_PATH%\submodules\vim\seoul256
+mklink /d vim\bundle\supertab            %CONFIG_PATH%\submodules\vim\supertab
+mklink /d vim\bundle\tern                %CONFIG_PATH%\submodules\vim\tern
+mklink /d vim\bundle\vim-airline         %CONFIG_PATH%\submodules\vim\vim-airline
+mklink /d vim\bundle\vim-fugitive        %CONFIG_PATH%\submodules\vim\vim-fugitive
+mklink /d vim\bundle\vim-gitgutter       %CONFIG_PATH%\submodules\vim\vim-gitgutter
+mklink /d vim\bundle\vim-javascript      %CONFIG_PATH%\submodules\vim\vim-javascript
+mklink /d vim\bundle\vim-man             %CONFIG_PATH%\submodules\vim\vim-man
+mklink /d vim\bundle\vim-signature       %CONFIG_PATH%\submodules\vim\vim-signature
+mklink /d vim\bundle\vim-spacebars       %CONFIG_PATH%\submodules\vim\vim-spacebars
+::mklink /d vim\bundle\editorconfig-vim  %CONFIG_PATH%\submodules\vim\editorconfig-vim
+::mklink /d vim\bundle\syntastic         %CONFIG_PATH%\submodules\vim\syntastic
+::mklink /d vim\bundle\ultisnips         %CONFIG_PATH%\submodules\vim\ultisnips
+
+mklink /d %HOME_DIR%\vimfiles            %CONFIG_PATH%\vim
+xcopy vim\vimrc_work %HOME_DIR%\.vimrc
+
+endlocal
+
+:: }}}
