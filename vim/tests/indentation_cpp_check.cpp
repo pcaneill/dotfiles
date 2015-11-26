37c750bd0f05dec2e510a253bd88bf193c9dc770
Pierre-Yves Caneill <pyc@moeryn.com>
2015-11-26 11:10:04 +0100

    VIM: adding the premises of vim configuration tests

---
+++ vim/tests/indentation_cpp_check.cpp
@@ -0,0 +1,45 @@
+// Header
+// Header
+// Header
+// Header
+
+#include <ddfks.h>
+#include <sdjfqksdf.h>
+
+#include "kqdfkjdskfj.h"
+#include "skdfjqkdjflqsdf.h"
+
+namespace toto {
+
+class Toto {
+public:
+   /** Test.
+    *
+    * Blabla.
+    */
+   Toto () {};
+   ~Toto () {};
+
+private:
+   void print_test () {};
+};
+
+static void func ()
+{
+   switch (dfdf) {
+      case toto: {
+      }  break;
+      case titi:
+         break;
+      default:
+         break;
+   }
+
+   if (AA && BB
+       && CC) {
+   }
+   else if () {
+   }
+}
+
+}
