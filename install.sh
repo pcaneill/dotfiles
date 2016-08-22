2bce0cf3e0b41ddb66f82e3e948b054221ec8bb8
Pierre-Yves Caneill <pyc@moeryn.com>
2016-08-22 21:09:01 +0200

    VIM: updating install.sh to reflect new vim configuration

--- install.sh
+++ install.sh
@@ -14,39 +14,6 @@ xfconf-query -c xfce4-desktop                                 \
 # }}}
 # {{{ Vim
 
-echo -ne "Initializing vim configuration ..."
-mkdir vim/plugin 2> /dev/null
-mkdir vim/doc    2> /dev/null
-mkdir vim/bundle 2> /dev/null
-
-ln -s ../../submodules/vim/YouCompleteMe      vim/bundle/YouCompleteMe     2> /dev/null
-ln -s ../../submodules/vim/a                  vim/bundle/a                 2> /dev/null
-ln -s ../../submodules/vim/badwolf            vim/bundle/badwolf           2> /dev/null
-ln -s ../../submodules/vim/bufexplorer        vim/bundle/bufexplorer       2> /dev/null
-ln -s ../../submodules/vim/ctrlp              vim/bundle/ctrlp             2> /dev/null
-ln -s ../../submodules/vim/delimitMate        vim/bundle/delimitMate       2> /dev/null
-ln -s ../../submodules/vim/editorconfig-vim   vim/bundle/editorconfig-vim  2> /dev/null
-ln -s ../../submodules/vim/gtags              vim/bundle/gtags             2> /dev/null
-ln -s ../../submodules/vim/htlm5              vim/bundle/htlm5             2> /dev/null
-ln -s ../../submodules/vim/neomake            vim/bundle/neomake           2> /dev/null
-ln -s ../../submodules/vim/neoterm            vim/bundle/neoterm           2> /dev/null
-ln -s ../../submodules/vim/nerdcommenter      vim/bundle/nerdcommenter     2> /dev/null
-ln -s ../../submodules/vim/nerdtree           vim/bundle/nerdtree          2> /dev/null
-ln -s ../../submodules/vim/seoul256           vim/bundle/seoul256          2> /dev/null
-ln -s ../../submodules/vim/supertab           vim/bundle/supertab          2> /dev/null
-ln -s ../../submodules/vim/syntastic          vim/bundle/syntastic         2> /dev/null
-ln -s ../../submodules/vim/tern               vim/bundle/tern              2> /dev/null
-ln -s ../../submodules/vim/ultisnips          vim/bundle/ultisnips         2> /dev/null
-ln -s ../../submodules/vim/vim-airline        vim/bundle/vim-airline       2> /dev/null
-ln -s ../../submodules/vim/vim-fugitive       vim/bundle/vim-fugitive      2> /dev/null
-ln -s ../../submodules/vim/vim-gitgutter      vim/bundle/vim-gitgutter     2> /dev/null
-ln -s ../../submodules/vim/vim-javascript     vim/bundle/vim-javascript    2> /dev/null
-ln -s ../../submodules/vim/vim-man            vim/bundle/vim-man           2> /dev/null
-ln -s ../../submodules/vim/vim-rtags          vim/bundle/vim-rtags         2> /dev/null
-ln -s ../../submodules/vim/vim-signature      vim/bundle/vim-signature     2> /dev/null
-ln -s ../../submodules/vim/vim-spacebars      vim/bundle/vim-spacebars     2> /dev/null
-ln -s ../../submodules/vim/vim-cpp-enhanced-highlight vim/bundle/syntax    2> /dev/null
-
 cp vim/vimrc_perso ~/.vimrc
 
 if [ ${IS_WORK} ]; then
@@ -55,12 +22,11 @@ if [ ${IS_WORK} ]; then
   cp vim/vimrc_work ~/.vimrc
 fi
 
-
 # {{{ Neovim
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 mkdir -p ${XDG_CONFIG_HOME}/nvim
-ln -s  ${ROOT}/vim/nvimrc_perso ${XDG_CONFIG_HOME}/nvim/init.vim
+ln -s  ${ROOT}/vim/vimrc ${XDG_CONFIG_HOME}/nvim/init.vim
 
 if  [ ${IS_WORK} ]; then
   ln -s  ${ROOT}/vim/nvimrc_work ${XDG_CONFIG_HOME}/nvim/init.vim
@@ -68,28 +34,17 @@ fi
 
 
 # }}}
-# {{{ YouCompleteMe
-
-cd vim/bundle/YouCompleteMe/
-script -q -c './install.py --clang-completer' > /dev/null
-if [ $? -ne 0 ]; then
-    echo "Unable to install YouCompleteMe"
-    exit
-fi
-cd $ROOT
-
-# }}}
 # {{{ Tern
-
-cd vim/bundle/tern/
-npm install > /dev/null
-cd node_modules/tern/plugin/
-wget https://raw.githubusercontent.com/Slava/tern-meteor/master/meteor.js 2> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Unable to get meteor.js tern plugin"
-    exit
-fi
-cd $ROOT
+#
+# cd vim/bundle/tern/
+# npm install > /dev/null
+# cd node_modules/tern/plugin/
+# wget https://raw.githubusercontent.com/Slava/tern-meteor/master/meteor.js 2> /dev/null
+# if [ $? -ne 0 ]; then
+#     echo "Unable to get meteor.js tern plugin"
+#     exit
+# fi
+# cd $ROOT
 # }}}
 # {{{ Rtags
 
