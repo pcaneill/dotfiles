c4f2b2f822d11371f85fdce5e802f410380f7f00
Pierre-Yves Caneill <pyc@moeryn.com>
2016-10-24 00:59:27 +0200

    IDEA: using jar instead of xml to use config

--- IntelliJ/import.sh
+++
@@ -1,9 +0,0 @@
-cp -r ~/$1/config/codestyles/* idea/config/codestyles
-cp -r ~/$1/config/colors/* idea/config/colors
-cp -r ~/$1/config/filetypes/* idea/config/filetypes
-cp -r ~/$1/config/inspection/* idea/config/inspection
-cp -r ~/$1/config/keymaps/* idea/config/keymaps
-cp -r ~/$1/config/options/* idea/config/options
-cp -r ~/$1/config/templates/* idea/options/templates
-cp -r ~/.ideavimrc ideavimrc
-
