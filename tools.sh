e3a0e28691bd20f5d74934a2210a8eae7e0e4cb4
Pierre-Yves Caneill <pyc@moeryn.com>
2017-10-29 21:39:55 +0100

    TOOLS: update tools instalation

--- tools.sh
+++ tools.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
 mkdir ~/tools
-cd ~/tools
-git clone http://github.com/neovim/neovim.git
-cd neovim
-make
-sudo make install
-
 
 cd ~/tools
 git clone https://github.com/equalsraf/neovim-qt.git
 cd neovim-qt
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make
-
-
-cd ~/tools
-git clone https://github.com/cgdb/cgdb.git
-cd cgdb
-./autogen.sh
-./configure --prefix=/usr/local
-make
-
+sudo make install
